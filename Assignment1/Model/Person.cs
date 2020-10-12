8694f1a28582044d088eeb39cf48908dbe4cc376
Bogdan Mezei <bogdan335@hotmail.com>
2020-10-12 19:39:52 +0200

    Fixed login - styled AddAdult.razor

--- Assignment1/Model/Person.cs
+++ Assignment1/Model/Person.cs
@@ -9,9 +9,12 @@ public class Person {
     
     public int Id { get; set; }
     [NotNull]
+    [Required(ErrorMessage = "First name is required.")]
     public string FirstName { get; set; }
     [NotNull]
+    [Required(ErrorMessage = "Last name is required.")]
     public string LastName { get; set; }
+    [NotNull]
     [ValidHairColor]
     public string HairColor { get; set; }
     [NotNull]
@@ -24,6 +27,7 @@ public class Person {
     [NotNull, Range(30, 250)]
     public int Height { get; set; }
     [NotNull]
+    [Required(ErrorMessage = "Gender is required.")]
     public string Sex { get; set; }
 
     public void Update(Person toUpdate) {
@@ -43,7 +47,9 @@ public class ValidHairColor : ValidationAttribute {
     protected override ValidationResult IsValid(object value, ValidationContext validationContext) {
         List<string> valid = new[] {"blond", "red", "brown", "black",
             "white", "grey", "blue", "green", "leverpostej"}.ToList();
-        if (valid == null || valid.Contains(value.ToString().ToLower())) {
+        if(value == null)
+            return new ValidationResult("Valid hair colors are: Blond, Red, Brown, Black, White, Grey, Blue, Green, Leverpostej");
+        if (valid.Contains(value.ToString().ToLower())) {
             return ValidationResult.Success;
         }
         return new ValidationResult("Valid hair colors are: Blond, Red, Brown, Black, White, Grey, Blue, Green, Leverpostej");
@@ -54,7 +60,9 @@ public class ValidEyeColor : ValidationAttribute {
     protected override ValidationResult IsValid(object value, ValidationContext validationContext) {
         List<string> valid = new[] {"brown", "grey", "green", "blue",
             "amber", "hazel"}.ToList();
-        if (valid != null && valid.Contains(value.ToString().ToLower())) {
+        if(value == null)
+            return new ValidationResult("Valid eye colors are: Brown, Grey, Green, Blue, Amber, Hazel");
+        if (valid.Contains(value.ToString().ToLower())) {
             return ValidationResult.Success;
         }
         return new ValidationResult("Valid eye colors are: Brown, Grey, Green, Blue, Amber, Hazel");
